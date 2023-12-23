8ef226b2ff56fc0bb7fde4535db977cc935892bf
Md Musfikur Rahman <107528767+Md-Musfikur-Rahman@users.noreply.github.com>
2023-12-23 19:32:33 +0600

    Credentials login

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,7 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
   providers: [
@@ -11,6 +12,36 @@ export const options: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_Client_ID as string,
       clientSecret: process.env.GOOGLE_Client_Secret as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "musfikur@test.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { email, password }: any = credentials;
+
+        if (email === "musfikur@test.com" && password === "musfikur") {
+          return null;
+        }
+
+        return { id: "011", name: "Musfikur", email: "musfikur@test.com" };
+        //   const res = await fetch("/your/endpoint", {
+        //     method: "POST",
+        //     body: JSON.stringify(credentials),
+        //     headers: { "Content-Type": "application/json" },
+        //   });
+        //   const user = await res.json();
+        //   if (res.ok && user) {
+        //     return user;
+        //   }
+        //   return null;
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
